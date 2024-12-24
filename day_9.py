612bde31db91794849a9f057fbae2c33d7be4b94
swativ5 <swativ.05.v@gmail.com>
2024-12-24 05:36:42 +0530

    day 9 completed and tried day 6 pt2 again

--- day_9.py
+++ day_9.py
@@ -2,7 +2,7 @@ from queue import PriorityQueue
 
 f = open("input/day9_input.txt", "r")
 puzzle = f.read().strip()
-puzzle = "2333133121414131402"
+# puzzle = "2333133121414131402"
 
 def part1():
     memory = []
@@ -36,41 +36,43 @@ def part1():
 
 part1()
 
-memory = []
+files = {}
+blanks = []
+
 id = 0
-for index in range(0, len(puzzle) - 1, 2):
-    block = []
-    for _ in range(int(puzzle[index])):
-        block.append(str(id))
-    if block != []:
-        memory.append(block)
-    free = []
-    for _ in range(int(puzzle[index + 1])):
-        free.append(".")
-    if free != []:
-        memory.append(free)
-    id += 1
-block = []
-for _ in range(int(puzzle[len(puzzle) - 1])):
-    block.append(str(id))
-if block != []:
-    memory.append(block)
-print(memory)
+position = 0
 
-pq = PriorityQueue()
-for index in range(len(memory)):
-    if memory[index][0] == ".":
-        pq.put(index)
+for i in range(len(puzzle)):
+    char = puzzle[i]
+    x = int(char)
+    if i % 2 == 0:
+        files[id] = (position, x)
+        id += 1
+    else:
+        blanks.append((position, x))
+    position += x
 
-for mindex in range(len(memory) - 1, -1, -1):
-    if memory[mindex][0] == ".":
-        continue
-    print(memory[mindex])
-    pindex = pq.get(0)
-    
-    plen = memory[pindex]
-    mlen = memory[mindex]
 
-    pass
-print(memory)
-# 00 99 2111777.44.333....5555.6666.....8888..
+while id > 0:
+    id -= 1
+    position, size = files[id]
+    for i in range(len(blanks)):
+        start, length = blanks[i]
+        if start >= position:
+            blanks = blanks[:i]
+            break
+        if size <= length:
+            files[id] = (start, size)
+            if size == length:
+                blanks.pop(i)
+            else:
+                blanks[i] = (start + size, length - size)
+            break
+total = 0
+
+for id, (position, size) in files.items():
+    for x in range(position, position + size):
+        total += id * x
+
+print(total)
+#6398065450842
